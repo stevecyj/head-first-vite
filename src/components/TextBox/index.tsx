99445666a4e5ca2311c33d5d04778379e97a1a2b
stevecyj <ycsteven.tsao@gmail.com>
2023-07-19 23:32:55 +0800

    emotion css composition

--- src/components/TextBox/index.tsx
+++ src/components/TextBox/index.tsx
@@ -8,8 +8,26 @@ const base = {
   },
 };
 
+const danger = {
+  color: "red",
+};
+
+const base2 = {
+  "background-color": "darkgreen",
+  color: "turquoise",
+};
 const TextBox = () => {
-  return <div css={base}>This has a hotpink background.</div>;
+  return (
+    <>
+      <div css={base}>This has a hotpink background.</div>
+      <div css={base2}>This will be turquoise</div>
+      <div css={[danger, base2]}>
+        This will be also be turquoise since the base styles overwrite the
+        danger styles.
+      </div>
+      <div css={[base2, danger]}>This will be red</div>
+    </>
+  );
 };
 
 export default TextBox;
