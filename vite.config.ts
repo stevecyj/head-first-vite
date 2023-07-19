4b3d80b76620a9f405d60b426def02fb107ae6db
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-07-19 12:56:33 +0800

    chore: refactor SCSS imports and configurations
    - Remove the import of `../../variable` in `src/components/Header/index.scss`
    - Rename `src/variable.scss` to `src/variables.scss`
    - Add imports and configurations related to SCSS in `vite.config.ts`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { normalizePath } from "vite";
+import path from "path";
+
+const resolve = (str: string) => normalizePath(path.resolve(__dirname, str));
+const variablePath = resolve("src/variables.scss");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
+});
