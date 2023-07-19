0051fe871487a52e9a2352b49b3b4006440288d7
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-07-19 16:20:23 +0800

    feat: refactor Button component and import statements
    - Add a new import statement for the Button component in App.tsx
    - Add a new Button component in the return statement of the App function in App.tsx
    - Create a new file for the Button component in the components folder
    - Add styled-components import statement in the Button component file
    - Define styles for the Button component in the Button component file
    - Add export statement for the Button component in the Button component file
    - Modify the return statement of the Header function in the Header component file to include a className prop
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
       <Header />
+      <Button>Click me</Button>
     </div>
   );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #fff;
+  border: 1px solid #000;
+  border-radius: 4px;
+  color: #000;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 700;
+  padding: 8px 16px;
+  transition: all 0.2s ease-in-out;
+  `;
+
+export default Button;
