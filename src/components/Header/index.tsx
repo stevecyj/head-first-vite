d711a38aa9a40a0836bf7b259222848238e0e3a6
stevecyj <ycsteven.tsao@gmail.com>
2023-07-26 07:02:36 +0800

    The label that best describes this change is "style".: update Header component styles and cache
    - Updated the `.stylelintcache` file
    - Modified the `src/components/Header/index.module.scss` file
    - Modified the `src/components/Header/index.tsx` file
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,11 @@
 import styles from "./index.module.scss";
 
+import logoSrc from "@assets/imgs/vite.png";
 export function Header() {
-  return <p className={styles.header}>This is Header</p>;
+  return (
+    <div className={`p-20px text-center ${styles.header}`}>
+      This is Header
+      <img className="m-auto mb-4" src={logoSrc} alt="" />
+    </div>
+  );
 }
