7fcc91ab4d78c7fa67fe3dce50e2bed496c1084e
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-07-27 17:52:05 +0800

    feat: refactor build process and add new plugins
    - Modify the `build.js` file
    - Add a new file `env-plugin.js`
    - Add a new file `httpImport-plugin.js`
    - Modify the `index.jsx` file
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,15 @@
 // esbuild
-import Server from "react-dom/server";
-const Greet = () => {
-  return <h1>hello, JOJO!</h1>;
-};
-console.log("Server", Server.renderToString(<Greet />));
+// import Server from "react-dom/server";
+// const Greet = () => {
+//   return <h1>hello, JOJO!</h1>;
+// };
+// console.log("Server", Server.renderToString(<Greet />));
+
+import { render } from "https://cdn.skypack.dev/react-dom";
+import React from "https://cdn.skypack.dev/react";
+import { zip } from "https://unpkg.com/lodash-es@4.17.15/lodash.js";
+console.log(zip([1, 2], ["a", "b"]));
+
+let Greet = () => <h1>Hello, juejin!</h1>;
+
+render(<Greet />, document.getElementById("root"));
