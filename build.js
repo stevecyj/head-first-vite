87c98f219733663afdc4fc6f3e15162fa3025445
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-07-27 15:11:54 +0800

    build: add build script for bundling and sourcemaps
    - Add a new file `build.js` with some code for building an application
    - Import the `esbuild` and `fs` modules
    - Configure `esbuild.build` with various options for bundling and sourcemaps
    - Set the `absWorkingDir` to the current working directory
    - Specify the entry point as `./src/index.jsx`
    - Enable bundling, splitting, and sourcemaps
    - Set `metafile` option to true to generate a metafile
    - Set the output format to `esm`
    - Add a loader for `.png` files using base64 encoding
    - Set the output directory to `./dist`
    - Write the resulting `metafile` to a file named `meta.json`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ build.js
@@ -0,0 +1,18 @@
+import * as esbuild from "esbuild";
+import fs from "node:fs";
+
+let result = await esbuild.build({
+  absWorkingDir: process.cwd(),
+  entryPoints: ["./src/index.jsx"],
+  bundle: true,
+  splitting: true,
+  sourcemap: true,
+  metafile: true,
+  format: "esm",
+  loader: {
+    ".png": "base64"
+  },
+  outdir: "./dist"
+});
+
+fs.writeFileSync("meta.json", JSON.stringify(result.metafile));
