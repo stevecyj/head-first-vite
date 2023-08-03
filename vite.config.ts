15a40b2ac224f4aa5386bd28ba343be4e997f4a9
stevecyj <ycsteven.tsao@gmail.com>
2023-08-03 18:20:40 +0800

    refactor: update command line argument pattern and index access
    - Modify the regular expression pattern for matching command line arguments
    - Adjust the index of `_argv` to access the correct element for matching the regular expression
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -26,8 +26,8 @@ function checkArgv(_argv: string[]) {
 }
 checkArgv(_argv);
 
-const regex = /^--theme=(\d+)$/;
-const match = _argv[1]?.match(regex);
+const regex = /^--themethemetheme=(\d+)$/;
+const match = _argv[3]?.match(regex);
 if (match) {
   console.log("theme =====> ", match[1]);
 } else {
